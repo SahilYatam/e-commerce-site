29a35c51636dfa921b38f7ff24876f86b95a28ca
Sahil Yatam <sahilyatam9@gmail.com>
2025-09-09 21:24:26 +0530

    feat(app): integrate auth and session routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,6 +6,9 @@ import cookieParser from "cookie-parser";
 import rateLimit from "express-rate-limit";
 import { errorHandler, notFoundHandler } from "./middlewares/globalErrorHandler.js";
 
+import authRouter from "./routers/auth.routes.js"
+import sessionRouter from "./routers/session.routes.js"
+
 const app = express();
 
 app.use(helmet());
@@ -21,6 +24,9 @@ const limiter = rateLimit({
   max: 100,
   message: "Too many requests, please try again later."
 })
+
+app.use("/api/v1/auth", limiter, authRouter);
+app.use("/api/v1/session", limiter, sessionRouter);
 
 app.use(errorHandler);
 app.use(notFoundHandler);
