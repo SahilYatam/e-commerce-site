27f46957ac4691f22219d9f4dbfa94f24075884a
Sahil Yatam <sahilyatam9@gmail.com>
2025-09-11 16:30:20 +0530

    feat(product-controller): add controller functions for product APIs

---
+++ backend/src/controllers/product.controller.js
@@ -0,0 +1,42 @@
+import { asyncHandler } from "../utils/handlers/asyncHandler.js"
+import { ApiResponse } from "../utils/responses/ApiResponse.js"
+
+import { productService } from "../services/product.service.js"
+
+const addProduct = asyncHandler(async(req, res) => {
+    const sellerId = req.user?.id
+    const product = await productService.addProduct(sellerId, req.body);
+
+    return res.status(200).json(new ApiResponse(200, {product}, "Product added in inventory"));
+});
+
+const deleteProduct = asyncHandler(async(req, res) => {
+    const productId = req.params.id;
+    const deletedProduct = await productService.deleteProduct(productId);
+
+    return res.status(200).json(new ApiResponse(200, {deletedProduct}, "Product deleted successfully"));
+});
+
+const updateProduct = asyncHandler(async(req, res) => {
+    const productId = req.params.id
+    const updatedProduct = await productService.updateProduct(productId, req.body);
+
+    return res.status(200).json(new ApiResponse(200, {updatedProduct}, "Product updated successfully"));
+});
+
+const getAllProducts = asyncHandler(async(req, res) => {
+    const {page, limit, total, products} = await productService.getAllProducts()
+
+    const meta = {page, limit, total};
+
+    const message = total === 0 ? "No products available in the catalog" : "Products retrieved from database"
+
+    return res.status(200).json(new ApiResponse(200, {products}, message, meta));
+});
+
+export const productController = {
+    addProduct,
+    deleteProduct,
+    updateProduct,
+    getAllProducts
+}
