1039da470b6a74f40ac37f3db59a6eb1e447e911
Sahil Yatam <sahilyatam9@gmail.com>
2025-09-12 20:28:09 +0530

    feat(product-routes): add product routes

---
+++ backend/src/routers/product.routes.js
@@ -0,0 +1,59 @@
+import Router from "express";
+import { productController } from "../controllers/product.controller.js";
+import { productValidation } from "../validators/product.validator.js";
+
+import { authentication } from "../middlewares/auth.middleware.js";
+import { authorizeRole } from "../middlewares/auth.middleware.js";
+import { validateRequest } from "../middlewares/joiValidation.middleware.js";
+
+const router = Router();
+
+router.post(
+  "/add-products",
+  authentication,
+  authorizeRole("seller"),
+  validateRequest(productValidation.addProductSchema),
+  productController.addProduct
+);
+
+router.delete(
+  "/delete/:id",
+  authentication,
+  authorizeRole("seller"),
+  validateRequest(productValidation.deleteProductSchema, "params"),
+  productController.deleteProduct
+);
+
+router.path(
+  "/update/:id",
+  authentication,
+  authorizeRole("seller"),
+  validateRequest(productValidation.updateProductSchema, "params"),
+  productController.updateProduct
+);
+
+router.get(
+  "/all-products",
+  validateRequest(productValidation.getAllProductsSchema),
+  productController.getAllProducts
+);
+
+router.get(
+  "/:id",
+  validateRequest(productValidation.getProductByIdSchema),
+  productController.getProductById
+);
+
+router.get(
+  "/product-category",
+  validateRequest(productValidation.getProductsByCategorySchema),
+  productController.getProductsByCategory
+);
+
+router.get(
+  "/search",
+  validateRequest(productValidation.searchProductsWithCategorySchema),
+  productController.searchProductsWithCategory
+);
+
+export default router;
