3cf8ba94ff1ef736306d12590de541abf73cd4f0
Sahil Yatam <sahilyatam9@gmail.com>
2025-09-12 19:58:16 +0530

    feat(product-service): implement product retrieval, filter, and search functionality

--- backend/src/services/product.service.js
+++ backend/src/services/product.service.js
@@ -66,10 +66,69 @@ const getAllProducts = async () => {
     return {page, limit, total, products};
 }
 
+const getProductById = async(id) => {
+    const product = await Product.findById(id)
+    if(!product) throw new ApiError(404, "Product not found");
+
+    return product
+}
+
+const getProductsByCategory = async (category) => {
+    const page = 1;
+    const limit = 10;
+    const skip = (page - 1) * limit;
+
+    const products = await Product.find({category}).skip(skip).limit(limit);
+    const total = await Product.countDocuments({category});
+    
+    return {page, limit, total, products};
+}
+
+const searchProducts = async(productName) => {
+    const name = productName.toLowerCase().trim()
+    
+    const page = 1;
+    const limit = 10;
+    const skip = (page - 1) * limit;
+
+    const searchResults = await Product.find({productName: {$regex: name, $options: "i"}}).skip(skip).limit(limit)
+
+    const totalSearch = await Product.countDocuments({productName: {$regex: name, $options: "i"}})
+
+    return {searchResults, page, limit, totalSearch};
+}
+
+const searchProductsWithCategory = async (productName) => {
+    // get search results
+    const {searchResults, page, limit, totalSearch} = await searchProducts(productName);
+
+    // if results found then get the category
+    let relatedCategoryResults = [];
+    if(searchResults.length > 0) {
+        const firstCategory = searchResults[0].category
+
+        relatedCategoryResults = await Product.find({
+            category: firstCategory,
+            _id: {$nin: searchResults.map(product => product._id)}
+        }).limit(limit)
+    }
+
+    const meta = {
+        page,
+        total: totalSearch,
+        searchCount: searchResults.length,
+        relatedCount: relatedCategoryResults.length,
+    }
+
+    return {searchResults, relatedCategoryResults, meta}
+}
 
 export const productService = {
     addProduct,
     deleteProduct,
     updateProduct,
-    getAllProducts
+    getAllProducts,
+    getProductById,
+    getProductsByCategory,
+    searchProductsWithCategory
 }
