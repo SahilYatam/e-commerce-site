86c4784b236e462ba73cc42fa02476d700e1ec3e
Sahil Yatam <sahilyatam9@gmail.com>
2025-09-13 12:10:23 +0530

    feat(order-routes): define routes for order management

---
+++ backend/src/routers/order.routes.js
@@ -0,0 +1,39 @@
+import { Router } from "express";
+
+import { orderController } from "../controllers/order.controller.js";
+import { orderValidator } from "../validators/order.validator.js";
+
+import { validateRequest } from "../middlewares/joiValidation.middleware.js";
+import { authentication } from "../middlewares/auth.middleware.js";
+import { authorizeRole } from "../middlewares/auth.middleware.js";
+
+const router = Router();
+
+router.post(
+  "/purchase/:id",
+  authentication,
+  validateRequest(orderValidator.buyProductSchema, "params"),
+  orderController.buyProduct
+);
+
+router.patch(
+  "/cancel-order/:id",
+  authentication,
+  validateRequest(orderValidator.cancelOrderSchema, "params"),
+  orderController.cancelOrder
+);
+
+router.get(
+  "/orders",
+  authentication,
+  authorizeRole("seller"),
+  orderController.getOrders
+);
+
+router.delete(
+  "/delete-order/:id",
+  authentication,
+  authorizeRole("seller"),
+  validateRequest(orderValidator.removeOrderSchema, "params"),
+  orderController.removeOrder
+);
